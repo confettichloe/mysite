ad93f0b9c8d7c82f56cb09651073b02b3203a129
confettichloe <41088772+confettichloe@users.noreply.github.com>
2018-07-10 15:17:19 -0700

    Add files via upload

---
+++ dynamic.js
@@ -0,0 +1,60 @@
+// var gif1 = document.getElementById("gif1");
+// var gif2 = document.getElementById("gif2");
+// var gif3 = document.getElementById("gif3");
+// var gif4 = document.getElementById("gif4");
+// var gif5 = document.getElementById("gif5");
+// var gif6 = document.getElementById("gif6");
+// var gif7 = document.getElementById("gif7");
+// var gif8 = document.getElementById("gif8");
+// var gif9 = document.getElementById("gif9");
+// var gif10 = document.getElementById("gif10");
+// var gif11 = document.getElementById("gif11");
+// var gif12 = document.getElementById("gif12");
+//
+// function change1() {
+//
+// }
+//
+// function change2() {
+//
+// }
+//
+// function change3() {
+//
+// }
+//
+// function change4() {
+//
+// }
+//
+// function change5() {
+//
+// }
+//
+// function change6() {
+//
+// }
+//
+// function change7() {
+//
+// }
+//
+// function change8() {
+//
+// }
+//
+// function change9() {
+//
+// }
+//
+// function change10() {
+//
+// }
+//
+// function change11() {
+//
+// }
+//
+// function change12() {
+//
+// }
